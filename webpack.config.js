d674923c021b12683fa44cfcfd8cc786c3716736
Thomas Babu <joseph.thomas.babu@gmail.com>
2016-06-25 12:22:20 +0800

    telling webpack to use babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,13 @@ var config = {
 	output : {
 		path : BUILD_DIR,
 		filename : 'bundle.js'
+	},
+	module : {
+		loaders : {
+			test : /\.jsx?/,
+			include : APP_DIR,
+			loader : 'babel'
+		}
 	}
 };
 
